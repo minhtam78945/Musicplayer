7fa748fcf6313255e07648db5662ebd61dbf315e
Minh Tâm <102682115+minhtam78945@users.noreply.github.com>
2022-09-17 13:01:39 +0700

    fixing

---
+++ app.js
@@ -0,0 +1,9 @@
+let song_left = document.getElementById("song_left");
+let song_right = document.getElementById("song_right");
+let song = document.getElementsByClassName("pro")[0];
+song_right.addEventListener("click", function () {
+    song.scrollLeft += 330;
+});
+song_left.addEventListener("click", function () {
+    song.scrollLeft -= 330;
+});
