f3488b40bb16abd7282e1ca623424f1e660343ed
Minh Tâm <102682115+minhtam78945@users.noreply.github.com>
2022-09-19 19:59:06 +0700

    update

--- JS/login.js
+++ JS/login.js
@@ -8,3 +8,37 @@ sign_up_btn.addEventListener("click", function () {
 sign_in_btn.addEventListener("click", function () {
     containe.classList.remove("active");
 });
+const from = document.querySelector(".sign-up-form")
+const email = from.querySelector(".email-field");
+const emailInput = email.querySelector("#email-sginup");
+const password = from.querySelector(".Create_password");
+const passwordInput = password.querySelector("#password-signup")
+const Confirm_pass = from.querySelector(".Confirm_Password");
+const Confirm_PasswordInput = Confirm_pass.querySelector("#cofirm-signup")
+
+console.log(from);
+// check Email
+function checkmail() {
+    const emialpattern = /^[^ ]+@[^ ]+\.[a-z]{2,3}$/;
+    if (!emailInput.value.match(emialpattern)) {
+        return email.classList.add("Invaild");
+    }
+    email.classList.remove("Invaild");
+}
+const eyeIcon = document.querySelectorAll(".fa-eye-slash");
+eyeIcon.forEach(eyeIcon => {
+    eyeIcon.addEventListener("click", function () {
+        const input = eyeIcon.parentElement.querySelector("input");
+        if (input.type === "password") {
+            eyeIcon.classList.replace("fa-eye-slash", "fa-eye");
+            return (input.type = "text");
+        }
+        eyeIcon.classList.replace("fa-eye", "fa-eye-slash");
+        input.type = "password";
+    });
+});
+from.addEventListener("submit", function (e) {
+    e.preventDefault();
+    checkmail()
+
+});
