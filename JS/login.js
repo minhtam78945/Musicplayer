9fa89ec10d72db1e2b14a12b17b5d3d04a6f9eda
Minh Tâm <102682115+minhtam78945@users.noreply.github.com>
2022-09-22 16:59:00 +0700

    update

--- JS/login.js
+++ JS/login.js
@@ -10,19 +10,39 @@ sign_in_btn.addEventListener("click", function () {
 });
 // OOp ELEMENT
 function Validator(object) {
+    function validate(InputElement, rule) {
+        var ErrorElement = InputElement.parentElement.querySelector(object.Error);
+        if (InputElement) {
+            InputElement.onblur = function () {
+                // get the value : InputElement.value;
+                // test fun : fun
+                var errorMEss = rule.test(InputElement.value);
+                if (errorMEss) {
+                    ErrorElement.innerText = errorMEss;
+                    InputElement.parentElement.classList.add("Invaild");
+                }
+                else {
+                    ErrorElement.innerText = "";
+                    InputElement.parentElement.classList.remove("Invaild");
+                }
+            }
+
+        }
+
+    }
     var formElement = document.querySelector(object.form);
     if (formElement) {
         object.rules.forEach(function (rule) {
             var InputElement = formElement.querySelector(rule.selector);
             if (InputElement) {
                 InputElement.onblur = function () {
-                    // get the value : InputElement.value;
-                    // test fun : fun
-                    var errorMEss = rule.test(InputElement.value);
-                    if (errorMEss) {
-                    }
+                    validate(InputElement, rule);
                 }
-                console.log(InputElement.parentElement.querySelector('.error'));
+                var ErrorElement = InputElement.parentElement.querySelector(object.Error);
+                InputElement.oninput = function () {
+                    ErrorElement.innerText = " ";
+                    InputElement.parentElement.classList.remove("Invaild");
+                }
             }
         });
     }
@@ -35,7 +55,7 @@ Validator.isRequired = function (selector) {
     return {
         selector: selector,
         test: function (value) {
-            return value.trim() ? undefined : 'Vui lòng nhập trường hợp này'
+            return value.trim() ? undefined : 'Please enter your account name'
 
         }
     }
@@ -44,6 +64,8 @@ Validator.isEmail = function (selector) {
     return {
         selector: selector,
         test: function (value) {
+            var rex = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
+            return rex.test(value) ? undefined : "Please enter valid email"
 
         }
     }
