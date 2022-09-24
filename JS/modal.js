a5d1d04bc45bf93d437911e0f152fc0481e9f99d
Minh Tâm <102682115+minhtam78945@users.noreply.github.com>
2022-09-24 21:34:46 +0700

    update

--- JS/modal.js
+++ JS/modal.js
@@ -1,14 +1,13 @@
 var openmodal = document.getElementById("btn-modal");
 console.log(openmodal);
 var modal = document.querySelector(".modal")
-var btnclose = document.getElementById("close");
+
 function hide(e) {
     modal.classList.toggle("hide");
 }
 openmodal.addEventListener("click", function () {
     modal.classList.toggle("hide");
 });
-btnclose.addEventListener("click", hide)
 modal.addEventListener("click", function (e) {
     if (e.target == e.currentTarget) {
         hide();
